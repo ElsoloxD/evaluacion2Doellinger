b2f77df3641f7ec2468fa9bbfb9fc09b9dc731a6
ElsoloxD <76150949+ElsoloxD@users.noreply.github.com>
2021-01-03 12:17:03 -0300

    Add files via upload

---
+++ js/recetario.js
@@ -0,0 +1,7 @@
+var ingrediente1 = prompt('ingrese 1 ingrediente para una receta')
+var ingrediente2 = prompt('ingrese otro ingrediente para una receta')
+var ingrediente3 = prompt('ingrese un ultimo ingrediente para una receta')
+document.write('<ul class="list-container"><li class="list">Primer ingrediente:<span> ' + ingrediente1+ '</span></li></ul>')
+document.write('<ul class="list-container"><li class="list">Segundo ingrediente:<span> ' + ingrediente2+ '</span></li></ul>')
+document.write('<ul class="list-container"><li class="list">Tercer ingrediente:<span> ' + ingrediente3+ '</span></li></ul>')
+document.write('<input type="text" placeholder="¿Que comida va a preparar?">' )
